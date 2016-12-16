87cbd289152b06d25423e6b2e1af130810a7f8a4
Daniel MacMillan <dmacmillan@dmacmillan01.phage.bcgsc.ca>
2016-12-16 13:02:22 -0800

    This version will work with a BED file containing UTRs and a mapping of transcript IDs to gene names. In the next version I will modify to look at one UTR at a time

--- tissueSpecific.py
+++ tissueSpecific.py
@@ -1,3 +1,5 @@
+__version__ = '1.0.0'
+
 import argparse
 import os
 import sys
@@ -5,8 +7,26 @@ import logging
 import time
 import random
 import colorsys
+import subprocess
+import pysam
 from Kleat import *
 from Matrix import *
+
+def genRHeatmap(gene, path, clust='TRUE'):
+    out = os.path.join(path, 'plot_{}.r'.format(gene))
+    with open(out, 'w') as f:
+        f.write(
+            'setwd("{}")\n' \
+            'library(pheatmap)\n' \
+            'library(RColorBrewer)\n' \
+            'mtx = read.table("{}")\n' \
+            'mybreaks = seq(min(mtx), max(mtx), by = 1)\n' \
+            'colors = rev(brewer.pal(max(min(length(mybreaks),11),3), "RdYlBu"))\n' \
+            'png("{}.png", width=1000, height=1000, res=150)\n' \
+            'pheatmap(mtx, main="{}", legend_breaks = mybreaks, cluster_rows = {}, cluster_cols = {})\n' \
+            'dev.off()'.format(path, gene, gene, gene, clust, clust)
+        )
+    return out
 
 def mapSamples(samplemap):
     mapping = {}
@@ -36,7 +56,7 @@ def getListIndexMinMetric(_list, linkage='centroid', metric='euclidian'):
 # and a threshold, return a
 # list of lists where each internal list
 # represents a cluster
-def iterAHC(_list, linkage='centroid', threshold=15):
+def iterAHC(_list, linkage='centroid', threshold=20):
     clusters = [[x] for x in _list]
     index, _min = getListIndexMinMetric(clusters)
     while (_min <= threshold) and (len(clusters) > 1):
@@ -45,9 +65,26 @@ def iterAHC(_list, linkage='centroid', threshold=15):
         index, _min = getListIndexMinMetric(clusters)
     return clusters
 
+#def outputClusters(clusters, tissues):
+#    lclusters = len(clusters)
+#    for i,cluster in enumerate(clusters):
+#        values = [x.cleavage_site for x in cluster]
+#        centroid = centroid(values)
+#        tissue = tissue.keys()[i]
+
+def groupUtr3s(utr3s):
+    d = {}
+    for utr in utr3s:
+        if utr.name not in d:
+            d[utr.name] = [utr]
+            continue
+        d[utr.name].append(utr)
+    return d
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Given KLEAT files identify tissue-specific cleavage sites as well as differentially utilized sites')
     parser.add_argument('kleats', nargs='+', help='One or more KLEAT files to analyze')
+    parser.add_argument('-u', '--utr3s', default='/home/dmacmillan/annotations/ensembl/ensembl.fixed.sorted.utr3s_only.genes.sorted.gz', help='Bgzipped tabix-indexed BED file containing all viable 3\'UTRs to use')
     parser.add_argument('-e', '--exclude', default=[], nargs='+', help='Tissue types to exclude')
     parser.add_argument('-s', '--samplemap', default=os.path.dirname(os.path.realpath(__file__))+'/samplemap', help='A file mapping sample names to tissue site')
     parser.add_argument('-n', '--normalize', action='store_true', help='Enable this to randomly subsample all tissues to equal the minimum number of samples')
@@ -56,6 +93,9 @@ if __name__ == '__main__':
     
     args = parser.parse_args()
     
+    utr3s = pysam.tabix_iterator(open(args.utr3s), parser=pysam.asBed())
+    gutr3s = groupUtr3s(utr3s)
+
     if not os.path.isdir(args.outdir):
         try:
             os.makedirs(args.outdir)
@@ -82,7 +122,7 @@ if __name__ == '__main__':
             continue
         if tissue not in tissues:
             tissues[tissue] = {
-                'file': open(os.path.join(args.outdir, '{}.bg'.format(tissue)), 'w'),
+                'file': None,#open(os.path.join(args.outdir, '{}.bg'.format(tissue)), 'w'),
                 'samples': set()
             }
         tissues[tissue]['samples'].add(sample)
@@ -96,16 +136,8 @@ if __name__ == '__main__':
     N = len(tissues)
     hsv = [(x*1.0/N, 1, 1) for x in range(N)]
     rgb = map(lambda x: colorsys.hsv_to_rgb(*x), hsv)
-    for i,tissue in enumerate(tissues):
-        tissues[tissue]['file'].write('track type=\'bedGraph\' visibility=\'2\' name=\'{}\' color=\'{}\'\n'.format(tissue, (',').join([str(x*255) for x in rgb[i]])))
-
-    # Normalize
-    #if args.normalize:
-    #    smallest = min([len(tissues[tissue]['samples']) for tissue in tissues])
-    #    logging.debug('smallest: {}'.format(smallest))
-    #    keep = set([x for y in [random.sample(tissues[tissue]['samples'], smallest) for tissue in tissues] for x in y])
-        #keep = set([x for x in random.sample(tissues[tissue]['samples'], smallest) for tissue in tissues])
-    #    kleats = [x for x in kleats if x.name in keep]
+    #for i,tissue in enumerate(tissues):
+    #    tissues[tissue]['file'].write('track type=\'bedGraph\' visibility=\'2\' name=\'{}\' color=\'{}\'\n'.format(tissue, (',').join([str(x*255) for x in rgb[i]])))
     
     kleats = Kleat.groupKleat(kleats)
 
@@ -116,11 +148,28 @@ if __name__ == '__main__':
             lclusters = len(clusters)
             mtx = Matrix(N, lclusters)
             centroids = []
+            myutr3 = None
             for cluster in clusters:
                 values = [x.cleavage_site for x in cluster]
                 centroids.append(centroid(values))
+            try:
+                utrs = gutr3s[gene]
+            except KeyError:
+                continue
+            if len(utrs) > 1:
+                for u in utrs:
+                    if all([(u.start-20 <= x <= u.end+20) for x in centroids]):
+                        myutr3 = u
+            else:
+                myutr3 = utrs[0]
+                if not all([(myutr3.start-20 <= x <= myutr3.end+20) for x in centroids]):
+                    continue
+            try:
+                lmyutr3 = myutr3.end - myutr3.start
+            except AttributeError:
+                continue
             for i in xrange(mtx.m):
-                tissue = tissues.keys()[i]
+                tissue = tkeys[i]
                 for j in xrange(mtx.n):
                     values = [x.cleavage_site for x in clusters[j] if mapping[x.name] == tissue]
                     if not values:
@@ -130,12 +179,26 @@ if __name__ == '__main__':
                     factor = len(tissues[tissue]['samples'])
                     if args.normalize:
                         score /= float(factor)
-                    tissues[tissue]['file'].write('{}\t{}\t{}\t{}\n'.format(chrom, cs-1, cs, score))
+                    #tissues[tissue]['file'].write('{}\t{}\t{}\t{}\n'.format(chrom, cs-1, cs, score))
                     mtx.mtx[i][j] = score
             mtx.rownames = [t.replace(' ', '_') for t in tissues.keys()]
-            mtx.colnames = ['c{}_{}'.format(i, int(x)) for i,x in enumerate(centroids)]
-            with open(os.path.join(args.outdir, gene), 'w') as f:
-                f.write(str(mtx))
+            #mtx.colnames = ['c{}_{}'.format(i, int(x)) for i,x in enumerate(centroids)]
+            if myutr3.strand == '+':
+                mtx.colnames = ['c_{}'.format(int(100*(x - myutr3.start)/(lmyutr3))) for x in centroids]
+            else:
+                mtx.colnames = ['c_{}'.format(int(100*(myutr3.end - x)/(lmyutr3))) for x in centroids]
+            for i in xrange(mtx.n):
+                col = mtx.col(i)
+                if len(col) - col.count(0) == 1:
+                    with open(os.path.join(args.outdir, gene), 'w') as f:
+                        f.write(str(mtx))
+                    path = os.path.abspath(args.outdir)
+                    if mtx.n == 1:
+                        rscript = genRHeatmap(gene, path, 'FALSE')
+                    else:
+                        rscript = genRHeatmap(gene, path)
+                    #print rscript
+                    #subprocess.Popen(['/gsc/btl/linuxbrew/bin/Rscript', rscript])
 #            deltas = Matrix(N, lclusters-1)
 #            for i in xrange(deltas.m):
 #                for j in xrange(deltas.n):
